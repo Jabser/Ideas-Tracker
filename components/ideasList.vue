f866e01bb2d73642c50f99cce1e33243d561b84c
Jakob <8946581+Jabser@users.noreply.github.com>
2024-10-20 01:51:19 +0200

    Update ideasList.vue
    Implemented the cards for the ideas with tailwind

--- components/ideasList.vue
+++ components/ideasList.vue
@@ -3,23 +3,28 @@ import { onMounted } from 'vue';
 
 const ideas = useIdeas();
 const user = useUserSession();
+
+import { TrashIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
-  <section class="u-margin-32">
-    <article class="card">
-      <h4 class="heading-level-4">Latest Ideas</h4>
-      <ul class="u-margin-block-start-8">
+  <section>
+    <article>
+      <h4 class="text-6xl font-semibold mb-4">Latest Ideas</h4>
+      <ul>
         <template v-if="ideas.current.value && ideas.current.value.length">
-          <li v-for="idea in ideas.current.value">
-            <div class="box">
-              <h5 class="heading-level-6">{{ idea.title }}</h5>
-              <p class="body-text-2">{{ idea.description }}</p>
-              <div class="u-position-absolute u-inset-inline-end-8 u-inset-block-start-8">
-                <button class="button is-small is-text is-only-icon" aria-label="Remove item" v-if="user.current.value &&
+          <li class="my-6 pb-6 border border-b-1 border-x-0 border-t-0" v-for="idea in ideas.current.value">
+            <div>
+              <h5 class="text-3xl font-semibold mb-2">{{ idea.title }}</h5>
+              <p class="">{{ idea.description }}</p>
+              <div class="mt-6">
+                <button class="text-sm font-semibold leading-6 py-3 px-7 text-white bg-red-600 hover:bg-red-800 transition ease-in-out duration-300 rounded-full" aria-label="Remove item" v-if="user.current.value &&
                   idea.userId === user.current.value.userId
                   " type="button" @click="ideas.remove(idea.$id)">
-                  <span class="icon-document-remove" aria-hidden="true" />
+                  <span aria-hidden="true" class="flex justify-center items-center">
+                    <TrashIcon class="size-5 mr-2" /> 
+                    Delete
+                  </span>
                 </button>
               </div>
             </div>
