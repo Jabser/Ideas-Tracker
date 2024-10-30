c9c743b3be674f8db38db312009bcc6f99ddeb7a
Jakob <8946581+Jabser@users.noreply.github.com>
2024-10-30 21:55:20 +0100

    Remade the ideaslist with tailwind

--- components/ideasList.vue
+++ components/ideasList.vue
@@ -10,15 +10,14 @@ import { TrashIcon } from '@heroicons/vue/24/outline'
 <template>
   <section>
     <article>
-      <h4 class="text-6xl font-semibold mb-4">Latest Ideas</h4>
+      <h4 class="text-5xl font-semibold mb-4">Latest Ideas</h4>
       <ul>
         <template v-if="ideas.current.value && ideas.current.value.length">
           <li class="my-6 py-6 border border-b-0 border-x-0 border-t-1" v-for="idea in ideas.current.value">
             <div>
               <h5 class="text-3xl font-semibold mb-2">{{ idea.title }}</h5>
               <p class="">{{ idea.description }}</p>
-              <div class="mt-6">
-                <button class="text-sm font-semibold leading-6 py-3 px-7 text-white bg-red-600 hover:bg-red-800 transition ease-in-out duration-300 rounded-full" aria-label="Remove item" v-if="user.current.value &&
+                <button class="mt-6 text-sm font-semibold leading-6 py-3 px-7 text-white bg-red-600 hover:bg-red-800 transition ease-in-out duration-300 rounded-full" aria-label="Remove item" v-if="user.current.value &&
                   idea.userId === user.current.value.userId
                   " type="button" @click="ideas.remove(idea.$id)">
                   <span aria-hidden="true" class="flex justify-center items-center">
@@ -26,7 +25,6 @@ import { TrashIcon } from '@heroicons/vue/24/outline'
                     Delete
                   </span>
                 </button>
-              </div>
             </div>
           </li>
         </template>
